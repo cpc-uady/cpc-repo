e089b4d1a3466c5068c98ce06d29fd581feb898a
miniyahirpro <132528029+miniyahirpro@users.noreply.github.com>
2024-02-19 22:07:37 -0600

    Add files via upload

---
+++ Solucion problemas semanales/Semana 1/Semana 1/Short Sort.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*SOLUCION: revisamos si alguna letra esta en su lugar, en caso de si, entonces es posible, en otro caso no es posible*/
+int main(){
+
+
+}
